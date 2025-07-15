7e87f1400cb31c1827fd55e84f50e96a472505f3
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-07-15 13:21:53 +0300

    Create game clicker

---
+++ Assets/Scripts/Ads/Advertisement.cs
@@ -0,0 +1,81 @@
+using System;
+using Playbox;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using Utils.Timer;
+
+public class Advertisement : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<Color> OnChangeRewardedButton;
+    [SerializeField] private float AdDuration = 1f;
+    [SerializeField] private UnityEvent OnRewardedAdReceived;
+    
+    private PlayboxTimer timer;
+    private bool isTimerReady = false;
+    
+    private bool isReady => Playbox.Advertisement.isReady() && isTimerReady;
+    
+    private void Awake()
+    {
+        timer = new PlayboxTimer();
+        timer.initialTime = AdDuration;
+        
+        MainInitialization.PostInitialization += () =>
+        {
+            Playbox.Advertisement.OnRewarderedReceived += OnRewarderedReceived;
+            Playbox.Advertisement.OnLoaded += AdvertisementOnOnLoaded;
+
+            timer.OnTimerStart += () =>
+            {
+                isTimerReady = false;
+                
+            };
+            
+            timer.OnTimeRemaining += f =>
+            {
+                isTimerReady = true;
+
+            };
+
+        };
+        
+        timer.Start();
+    }
+
+    private void AdvertisementOnOnLoaded()
+    {
+        
+    }
+
+    private void OnRewarderedReceived()
+    {
+        OnRewardedAdReceived?.Invoke();
+    }
+
+    private void CheckReadyStatus()
+    {
+        if (isReady)
+        {
+            OnChangeRewardedButton?.Invoke(Color.green);
+        }
+        else
+        {
+            OnChangeRewardedButton?.Invoke(Color.white);
+        }
+    }
+
+    private void Update()
+    {
+        timer.Update(Time.deltaTime);
+        CheckReadyStatus();
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (isReady)
+        {
+            Playbox.Advertisement.Show();
+        }
+    }
+}
